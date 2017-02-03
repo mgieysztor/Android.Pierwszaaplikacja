1d9c9f2ddab5895e98ae183ca4adc550f691f594
mgieysztor <mgiey@op.pl>
2017-02-03 20:40:51 +0100

    Wypchnięcie zawartości przed sobotnimi zajęciami.

--- app/src/main/java/com/sdacademy/gieysztor/michal/pierwszaaplikacja/HelloActivity.java
+++ app/src/main/java/com/sdacademy/gieysztor/michal/pierwszaaplikacja/HelloActivity.java
@@ -1,17 +1,28 @@
 package com.sdacademy.gieysztor.michal.pierwszaaplikacja;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class HelloActivity extends AppCompatActivity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello);
 
-        TextView textView = (TextView) findViewById(R.id.helloText);
+        textView = (TextView) findViewById(R.id.helloText);
+
+        Intent intent = getIntent();
+        if (intent != null) {
+            String surname = intent.getStringExtra(SDActivity.KEY_SURNAME);
+            String name = intent.getStringExtra(SDActivity.KEY_NAME);
+            textView.setText("Witaj " + name + " " + surname + "!");
+        }
+
 
     }
 }
--- app/src/main/java/com/sdacademy/gieysztor/michal/pierwszaaplikacja/SDActivity.java
+++ app/src/main/java/com/sdacademy/gieysztor/michal/pierwszaaplikacja/SDActivity.java
@@ -7,40 +7,91 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class SDActivity extends AppCompatActivity {
+public class SDActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final String KEY_NAME = "name";
+    public static final String KEY_SURNAME = "surname";
     private TextView textView;
-    private EditText editText;
+    private EditText nameText;
+    private EditText surnameText;
     private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { //to się bierze z cyklu życia activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sd);
-
         bindViews();
-        textView.setText("Witaj Jan Kowalski"); //praktycznie to co string
-
-        setListener();
-
+        setListeners();
+        textView.setText("Witaj Jan Kowalski!");
     }
 
     private void bindViews() {
-        textView = (TextView) findViewById(R.id.firsttext);  //R. jest skrótem od resources
-        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.firstText);
+        nameText = (EditText) findViewById(R.id.editTextName);
+        surnameText = (EditText) findViewById(R.id.editTextSurname);
         button = (Button) findViewById(R.id.nextButton);
     }
 
-    private void setListener() {
+    private void setListeners() {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SDActivity.this, HelloActivity.class);
-                String s = editText.getText().toString();
-                intent.putExtra("surname ", s);
-                startActivity(intent);
+                if (checkFields()){
+                    openHelloActivity();
+                }
+
+//                if (surnameText.getText().toString()==null){
+//                    surnameText.setError("errorSurnameName");
+//                } else if (nameText.getText().toString()==null){
+//                    nameText.setError("errorName");
+//                } else {
+//                    openHelloActivity();
+//
+////                    Toast.
+////                            makeText(this,"Podaj poprawne dane",Toast.LENGTH_SHORT).show();
+//                }
+
             }
         });
 
+//        button.setOnClickListener(this);  //nie wiem dlaczego wyremowałem
+    }
+
+    private boolean checkFields() {
+        String name = nameText.getText().toString();
+        String surname = surnameText.getText().toString();
+        if (name == null || name.isEmpty()){
+            nameText.setError("Wypełnij pole");
+            return false;
+        }
+        if (surname == null || surname.isEmpty()){
+            surnameText.setError("Wypełnij pole");
+            return false;
+        }
+        return true;
+    }
+
+    private void openHelloActivity() {
+        Intent intent = new Intent(SDActivity.this, HelloActivity.class);
+
+        String s = surnameText.getText().toString();
+        intent.putExtra(KEY_SURNAME, s);
+//        intent.putExtra(Intent.EXTRA_TEXT, s);
+//        intent.putExtra("surname", s);              //zmiana klucza poniższa linia jest alternatywnym zapisem
+
+        String n = nameText.getText().toString();
+        intent.putExtra(KEY_NAME,n);
+
+        startActivity(intent);
+
+        Toast.makeText(this,"lol",Toast.LENGTH_SHORT).show();
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        openHelloActivity();
     }
 }
